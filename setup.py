3a439423455059698034d857f7babaf257b12b61
Mario Steinhoff <mario.steinhoff@catapullt.de>
2013-11-17 21:31:35 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vmfusion',
-    version='0.1.0',
+    version='0.1.1',
     author='Mario Steinhoff',
     author_email='steinhoff.mario@gmail.com',
     packages=['vmfusion'],
