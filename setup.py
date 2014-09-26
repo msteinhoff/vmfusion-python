0d028bbb3d1347424372a567b79f6f7f03307953
Mario Steinhoff <mario.steinhoff@catapullt.de>
2014-09-26 13:57:36 +0200

    Use README.md file in setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/msteinhoff/vmfusion-python',
     license='LICENSE.txt',
     description='A python API for the VMware Fusion CLI tools.',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         "pyparsing >= 2.0.1"
     ]
